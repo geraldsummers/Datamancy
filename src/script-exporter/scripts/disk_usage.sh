57d834d9b040bf3ca0adf4cac2280a4bb5398986
gerald <laughlanfahey@gmail.com>
2025-10-24 17:11:32 +1100

    rebuild

--- src/script-exporter/scripts/disk_usage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Example script: Check disk usage
-# Returns disk usage percentage as a metric
-
-set -e
-
-USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
-
-echo "disk_usage_percent $USAGE"
